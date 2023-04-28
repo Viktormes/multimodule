c148b4eeb8da194a298c2d803899fcf9bb709d97
Viktormes <vikhag0722@gmail.com>
2023-04-28 12:55:07 +0200

    add ServiceLoader to Consumer.java

--- consumer/src/main/java/org/example/consumer/Consumer.java
+++ consumer/src/main/java/org/example/consumer/Consumer.java
@@ -2,10 +2,19 @@ package org.example.consumer;
 
 import org.example.service.TopScorer;
 
+import java.util.ServiceLoader;
+
 public class Consumer {
 
     public static void main(String[] args) {
-        TopScorer topScorer;
+
+
+        ServiceLoader<TopScorer> topScorers = ServiceLoader.load(TopScorer.class);
+
+        for (var topScorer: topScorers) {
+            System.out.println(topScorer.showTopScorer());
+
+        }
 
 
     }
